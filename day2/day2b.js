f2c22abee03bd2b347629d0640b9ff0598942df4
Chris Brunner <cbrunner@newrelic.com>
2022-12-02 15:17:05 -0800

    General cleanup

--- day2/day2b.js
+++ day2/day2b.js
@@ -8,17 +8,16 @@ const sumLine = (total, line) => {
   // Y = Draw
   // Z = Win
   [e, g] = line.split(' ');
+  if (e == 'B' && g == 'X') total += 1
+  if (e == 'C' && g == 'X') total += 2
   if (e == 'A' && g == 'X') total += 3
   if (e == 'A' && g == 'Y') total += 4
-  if (e == 'A' && g == 'Z') total += 8
-  if (e == 'B' && g == 'X') total += 1
   if (e == 'B' && g == 'Y') total += 5
-  if (e == 'B' && g == 'Z') total += 9
-  if (e == 'C' && g == 'X') total += 2
   if (e == 'C' && g == 'Y') total += 6
+  if (e == 'B' && g == 'Z') total += 9
   if (e == 'C' && g == 'Z') total += 7
+  if (e == 'A' && g == 'Z') total += 8
   return total
-  throw new Error("Should have exited by now");
 }
 const parseInput = (data) => {
   const strArray = data.split('\n');
