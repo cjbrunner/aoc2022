5aa063faab64e2d8bd73077f557d63a952da31c9
Chris Brunner <cbrunner@newrelic.com>
2022-12-06 16:29:42 -0800

    Linting

--- day1/day1a.js
+++ day1/day1a.js
@@ -1,34 +1,32 @@
 const fs = require('fs');
 
 const parseInput = (data) => {
-  strArray = data.split('\n');
-  elfObj = {'0': 0}
-  elfNum = 0
+  const strArray = data.split('\n');
+  const elfObj = { '0': 0 };
+  let elfNum = 0;
   strArray.forEach(e => {
     if (e.length > 0) {
-      elfObj[elfNum] = elfObj[elfNum] + parseInt(e);
+      elfObj[elfNum] += parseInt(e, 10);
     } else {
-      elfNum = elfNum+1;
+      elfNum += 1;
       elfObj[elfNum] = 0;
     }
-  })
+  });
   return elfObj;
 };
 
-const readData = (inputFile, fileType='utf8') => {
+const readData = (inputFile, fileType = 'utf8') => {
   try {
     return fs.readFileSync(inputFile, fileType);
   } catch (err) {
     console.error(err);
-    throw new Error("Error reading file");
+    throw new Error('Error reading file');
   }
-}
+};
 
-const findMaxElf = (input) => {
-  return Math.max.apply(Math, Object.values(input))
-}
+const findMaxElf = (input) => Math.max.apply(...Object.values(input));
 
-const rawData = readData('input.txt')
-const parsedInput = parseInput(rawData)
-const maxElf = findMaxElf(parsedInput)
-console.log(maxElf)
+const rawData = readData('input.txt');
+const parsedInput = parseInput(rawData);
+const maxElf = findMaxElf(parsedInput);
+console.log(maxElf);
